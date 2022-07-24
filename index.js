9d0b7adb1d8b2123c475eb90ba00a570e3cc3f11
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-07-25 00:38:57 +0500

    whale tracke bot complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your whale tracker script here!
+// import { ethers } from 'ethers';
+const ethers = require('ethers');
+// const player = require('play-sound')(opts = {})
+const CONTRACT_ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }]
+
+const CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+
+const rpcURL = "https://cloudflare-eth.com"
+const provider = new ethers.providers.JsonRpcProvider(rpcURL);
+
+const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider)
+
+// const playSound = () => {
+//     player.play('ding.mp3', function (err) {
+//         if (err) throw err
+//     })
+// }
+
+const TRANSFER_THRESHOLD = 1000000000000 // 100,000 USDC
+const main = async () => {
+    // playSound()
+    const block = await provider.getBlockNumber()
+    const name = await contract.name()
+    console.log(`Whale Tracker started\nListening for large transfers on ${name}`)
+
+    contract.on('Transfer', (from, to, value, data) => {
+
+        if (value.toNumber() >= TRANSFER_THRESHOLD) {
+            // playSound()
+            console.log(`New Whale Transfer for ${name}: https://etherscan.io/tx/${data.transactionHash} `)
+            //     console.log(from, to, value.toNumber())
+        }
+
+    })
+}
+
+main()
